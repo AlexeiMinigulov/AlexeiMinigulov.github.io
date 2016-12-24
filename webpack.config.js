fdb81ccb62c14dcd6c777530803e94cfea98c3a4
AlexeiMinigulov <leha.minigulov@gmail.com>
2016-12-24 11:11:54 +0400

    Add

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
     path = require('path'),
     sourcePath = path.join(__dirname, '/src'),
     nodeModules = path.join(__dirname, '/node_modules'),
@@ -14,35 +15,43 @@ module.exports = {
     entry: {
         app: ['./app.js']
     },
-
     output: {
         path: outputPath + '/js',
+        publicPath: '/',
         filename: '[name].js'
     },
 
-    watch: (NODE_ENV == 'development'),
 
+    watch: (NODE_ENV == 'development'),
     watchOptions: {
         aggregateTimeout: 200
     },
 
+
     devtool: (NODE_ENV == 'development') ? "source-map" : null,
+
 
     plugins: [
         new webpack.DefinePlugin({
             NODE_ENV: JSON.stringify(NODE_ENV)
-        })
+        }),
+        new webpack.ProvidePlugin({
+            /*$: "jquery/dist/jquery.min.js",
+            angular: "angular/angular.min.js",
+            materialize: "materialize-css/dist/js/materialize.min.js"*/
+        }),
+        new ExtractTextPlugin("../css/style.css")
     ],
+
+
     module: {
         loaders: [
             {
                 test: /\.js$/,      // Преобразовать в старый js
                 exclude: [      // Исключить
-                    /node_modules/,
-                    /bower_components/
+                    /node_modules/
                 ],
                 loader: 'babel'
-                //loader: 'babel?optional[]=runtime'
             },
             {
                 test: /\.html$/,    // встраиваем html в js script
@@ -53,24 +62,28 @@ module.exports = {
                 loader: 'ng-cache?prefix=[dir]/[dir]'
             },
             {
-                test: /\.css$/,         // Встраиваем стили в html
-                exclude: [      // Исключить
-                    /node_modules/,
-                    /bower_components/
-                ],
-                include: path.resolve(__dirname, "src"),
-                loader: 'style-loader!css-loader!autoprefixer-loader?browsers=last 2 versions'
+                test: /\.css$/,
+                exclude: [ path.resolve(__dirname, 'src/common.css') ],
+                loader: 'style!css!autoprefixer?browsers=last 2 versions'
             },
             {
-                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,    // При загрузки background стилей
-                exclude: [      // Исключить
-                    /node_modules/,
-                    /bower_components/
-                ],
-                include: path.resolve(__dirname, "src"),
-                loader: 'file?name=[path][name].[ext]'
+                test: /\.css$/,
+                include: path.resolve(__dirname, "src/common.css"),
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,   // При загрузки background стилей
+                include: [ path.resolve(__dirname, "node_modules/font-awesome") ],
+                loader: 'url?name=[path].[ext]&limit=80000'
+            },
+            {
+                test: /\.(svg|ttf|eot|otf|woff|woff2)$/,
+                exclude: [ path.resolve(__dirname, "node_modules/font-awesome") ],
+                loader: 'url'
             }
         ],
+
+
 
         devServer: {
             contentBase: path.resolve(__dirname, 'dist'),
